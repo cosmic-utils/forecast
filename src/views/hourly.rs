3bf4d8338b77fcf3acf05d9abd5ae37940c7f0c9
jwestall <jlwdrums94@gmail.com>
2024-08-28 22:13:27 -0500

    Added data to hourly view

--- src/views/hourly.rs
+++ src/views/hourly.rs
@@ -9,10 +9,19 @@ where
 {
     pub fn view_hourly_forecast(&self) -> Element<Message> {
         let location = self.config.location.clone();
+        let data = &self.weather_data.properties.timeseries;
+        let imediate_data = if data.len() == 0 {
+            // View is initalized before weather_data is loaded
+            // Need to pass dummy data to prevent crashing
+            0 as f64
+        } else {
+            data[0].data.instant.details.air_temperature.unwrap()
+        };
+
         column()
             .spacing(24)
             .push(text::title1(location.unwrap_or("Unknown".to_string())))
-            .push(text("Hourly View will appear here."))
+            .push(text(format!("{} degrees", imediate_data)))
             .into()
     }
 }
