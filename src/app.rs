424eb7d543a7ad35019bb52d1a51c6a625e95c10
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-09-03 18:38:38 +0200

    fix: prevent unwraping unset values

--- src/app.rs
+++ src/app.rs
@@ -478,32 +478,28 @@ where
     }
 
     fn update_weather_data(&self) -> Command<Message> {
-        let coords = (
-            self.config
-                .latitude
-                .as_ref()
-                .unwrap()
-                .parse::<f64>()
-                .expect("Error parsing string to f64"),
-            self.config
-                .longitude
-                .as_ref()
-                .unwrap()
-                .parse::<f64>()
-                .expect("Error parsing string to f64"),
-        );
+        if let (Some(lat), Some(long)) = (
+            self.config.latitude.as_ref(),
+            self.config.longitude.as_ref(),
+        ) {
+            let coords = (
+                lat.parse::<f64>().expect("Error parsing string to f64"),
+                long.parse::<f64>().expect("Error parsing string to f64"),
+            );
 
-        Command::perform(WeatherData::get_weather_data(coords), |data| match data {
-            Ok(data) => {
-                let Some(data) = data else {
-                    return cosmic::app::Message::App(Message::Error(
-                        "Could not get weather data.".to_string(),
-                    ));
-                };
-                cosmic::app::Message::App(Message::SetWeatherData(data.clone()))
-            }
-            Err(err) => cosmic::app::Message::App(Message::Error(err.to_string())),
-        })
+            return Command::perform(WeatherData::get_weather_data(coords), |data| match data {
+                Ok(data) => {
+                    let Some(data) = data else {
+                        return cosmic::app::Message::App(Message::Error(
+                            "Could not get weather data.".to_string(),
+                        ));
+                    };
+                    cosmic::app::Message::App(Message::SetWeatherData(data.clone()))
+                }
+                Err(err) => cosmic::app::Message::App(Message::Error(err.to_string())),
+            });
+        };
+        Command::none()
     }
 
     fn about(&self) -> Element<Message> {
