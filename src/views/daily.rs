01d7b3fc5db873a42ee94cae7779b019dbd646e3
jwestall <jlwdrums94@gmail.com>
2024-09-08 05:39:48 -0500

    Work on details screen

--- src/views/daily.rs
+++ src/views/daily.rs
@@ -1,10 +1,15 @@
 use chrono::Local;
+use cosmic::iced::Alignment;
+use cosmic::iced_widget::scrollable::Direction;
+use cosmic::iced_widget::scrollable::Properties;
 use cosmic::prelude::CollectionWidget;
 use cosmic::widget;
 use cosmic::Element;
 
 use crate::app::config::TimeFmt;
 use crate::app::{App, Message};
+use crate::model::weather::Details;
+use crate::model::weather::Timeseries;
 use crate::model::weather::WeatherData;
 
 impl App
@@ -41,6 +46,47 @@ where
                 .to_string(),
         };
 
+        let timeseries: Vec<Element<Message>> =
+            weather_data
+                .properties
+                .timeseries
+                .iter()
+                .filter(|timeseries| timeseries.time > current_time && timeseries.time.format("%H").to_string() == "12")
+                .map(|ts| {
+                    let data_6_hrs = match &ts.data.next_6_hours {
+                        Some(data) => match &data.details {
+                            Some(details) => details,
+                            None => &Details::default()
+                        },
+                        None => &Details::default()
+                    };
+
+                    widget::column()
+                        .align_items(Alignment::Center)
+                        .padding(spacing.space_xs)
+                        .spacing(spacing.space_xs)
+                        .push(widget::text(self.format_date(ts)))
+                        .push_maybe(ts.data.next_12_hours.as_ref().map(|next_12_hours| {
+                            let symbol = next_12_hours.summary.symbol_code.clone();
+                            widget::icon(WeatherData::icon_handle(symbol)).size(50)
+                        }))
+                        .push_maybe(data_6_hrs.air_temperature_max.map(
+                            |air_temperature_max| {
+                                widget::text(format!("{}°", self.set_temp_units(air_temperature_max)))
+                                    .size(24)
+                                    .style(cosmic::style::Text::Accent)
+                            },
+                        ))
+                        .push_maybe(data_6_hrs.air_temperature_min.map(
+                            |air_temperature_min| {
+                                widget::text(format!("{}°", self.set_temp_units(air_temperature_min)))
+                                    .size(24)
+                            },
+                        ))
+                        .into()
+                })
+                .collect();
+
         let column = widget::column()
             .padding(spacing.space_xs)
             .spacing(spacing.space_xs)
@@ -71,11 +117,19 @@ where
                             )),
                     ),
             )
+            .push(
+                widget::scrollable(widget::row::with_children(timeseries))
+                    .direction(Direction::Horizontal(Properties::default())),
+            )
             .push(widget::text(format!("Last updated: {}", last_updated)))
             .push(widget::text(
                 "Weather data from the Norwegian Meteorological Institute",
             ));
 
         column.into()
+    }
+
+    fn format_date(&self, ts: &Timeseries) -> String {
+        ts.time.format("%a").to_string()
     }
 }
